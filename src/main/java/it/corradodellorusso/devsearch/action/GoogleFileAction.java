af088f13fa0f5a326b0aa8ecd09a413e30c87c48
Giacinto Mancino <mancino.giacinto@gmail.com>
2020-05-11 19:39:19 +0200

    Implementation of Google search on files

---
+++ src/main/java/it/corradodellorusso/devsearch/action/GoogleFileAction.java
@@ -0,0 +1,32 @@
+package it.corradodellorusso.devsearch.action;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import it.corradodellorusso.devsearch.action.common.FileAction;
+import it.corradodellorusso.devsearch.search.GoogleFileSearch;
+import it.corradodellorusso.devsearch.search.Search;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Builds object to search jar files on <a href="https://google.com/" target="_blank">Google</a>.
+ */
+public class GoogleFileAction extends FileAction {
+
+    private static final String ENTRY_TEXT = "Search on Google";
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        String filename = getFile(event).getName();
+        Search search = new GoogleFileSearch(filename);
+        dispatch(search);
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent event) {
+        boolean shouldShow = hasFileAttached(event);
+        event.getPresentation().setEnabledAndVisible(shouldShow);
+        if (!shouldShow) {
+            return;
+        }
+        event.getPresentation().setText(ENTRY_TEXT);
+    }
+}
