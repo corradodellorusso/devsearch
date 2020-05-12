320c68fcd0046bb917db3a84c0bda998f38d936c
Corrado Dello Russo <info@corradodellorusso.it>
2020-05-12 16:39:07 +0200

    Icon and description.

---
+++ src/main/java/icons/DevSearchIcons.java
@@ -0,0 +1,11 @@
+package icons;
+
+import com.intellij.icons.AllIcons;
+
+import javax.swing.*;
+
+public class DevSearchIcons {
+
+    public final static Icon SEARCH_ICON = AllIcons.Actions.Search;
+
+}
